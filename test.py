b9c98d2e332d4994c4a9bfd378451ddd6cc32578
our1314 <our1314@foxmail.com>
2022-09-26 13:12:16 +0800

    GG

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ from models.classify_net1 import classify_net1
 
 names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
          'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.']
-img_path = "D:/桌面/ocr/Y/2022-09-23_09-58-25_+0800_3464.png"
+img_path = "C:/Users/pc/Desktop/ocr/Y/2022-09-23_09-58-22_+0800_3326.png"
 img = Image.open(img_path)
 img.show()
 print(img)
@@ -48,7 +48,7 @@ gg = torchvision.transforms.ToPILImage()(img[0, :, :, :])
 gg.show()
 print(img.shape)
 
-path = 'out/2022.09.25_20.35.19-epoch=128-loss=0.18743-acc=1.0.pth'
+path = 'run/train/epoch=13-train_acc=1.0.pth'
 checkpoint = torch.load(path)
 net = classify_net1()
 # model = torch.load(checkpoint['net'])
