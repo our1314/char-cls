5bd629fb7b5e79cca04849e54c68d26e3a4bc894
our1314 <our1314@foxmail.com>
2022-10-05 08:55:27 +0800

    修改注释

--- test2.py
+++ test2.py
@@ -2,15 +2,15 @@ import torch
 
 loss_fn = torch.nn.CrossEntropyLoss()
 
-a = torch.Tensor([[[1.0, 2.0, 3.0]]])
-b = torch.Tensor([[[1.0, 2.0, 3.1]]])
+a = torch.Tensor([[[0.3, 0.4, 0.3]]])
+b = torch.Tensor([[[0.29, 0.4, 0.31]]])
 l1 = loss_fn(a, b)
 pass
-a = torch.Tensor([[1.1, 2.0, 3.0]])
-b = torch.Tensor([[1.0, 2.0, 3.1]])
+a = torch.Tensor([[0.3, 0.4, 0.3]])
+b = torch.Tensor([[0.299, 0.4, 0.301]])
 l2 = loss_fn(a, b)
 pass
-a = torch.Tensor([[1.0, 2.0, 3.0]])
-b = torch.Tensor([[1.001, 2.0, 3.001]])
+a = torch.Tensor([[0.3, 0.4, 0.3]])
+b = torch.Tensor([[0.31, 0.38, 0.31]])
 l3 = loss_fn(a, b)
 pass
